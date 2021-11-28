faae4a32ce1ab1c9c8e231ea54c804f781d4d67c
Yordan Zhelyazkov <83996110+JordanZhelyazkov@users.noreply.github.com>
2021-11-28 17:16:05 +0200

    Update MemeGenerator.js

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -1,12 +1,13 @@
-import React from "react";
-import { useEffect, useState } from "react";
 export default function MemeGenerator() {
-  const [topText, setTopText] = useState("");
-  const [bottomText, setBottomText] = useState("");
+
+  const [text, setText] = useState({
+    topText: "",
+    bottomText: "",
+  })
   const [randomImg, setRandomImg] = useState(
     "https://www.pngall.com/wp-content/uploads/2016/03/Mother-Of-God-Meme-PNG.png"
   );
-  const [imagesArr, setImagesArr] = useState("");
+  const [imagesArr, setImagesArr] = useState([]);
 
   useEffect(() => {
     fetch("https://api.imgflip.com/get_memes")
@@ -17,14 +18,28 @@ export default function MemeGenerator() {
       });
   }, [imagesArr]);
 
+  function handleChange(event){
+    const {name,value} = event.target;
+    setText(prevText => {
+      return {
+      ...prevText,
+       [name]: value
+      }
+    })
+  }
+
   function handleSubmit(e) {
     e.preventDefault();
     let len = imagesArr.length;
     const randNum = Math.floor(Math.random() * len);
     const randomMeme = imagesArr[randNum].url;
     setRandomImg(randomMeme);
-    setTopText("");
-    setBottomText("");
+    setText(prevText => {
+      return {
+      topText: "",
+      bottomText: "",
+      }
+    });
   }
 
   return (
@@ -34,17 +49,19 @@ export default function MemeGenerator() {
       <form className="form" onSubmit={handleSubmit}>
 
         <input
+          name="topText"
           type="text"
-          value={topText}
+          value={text.topText}
           placeholder="type something"
-          onChange={(e) => setTopText(e.target.value)}
+          onChange={handleChange}
         />
 
         <input
+          name="bottomText"
           type="text"
-          value={bottomText}
+          value={text.bottomText}
           placeholder="type something"
-          onChange={(e) => setBottomText(e.target.value)}
+          onChange={handleChange}
         />
 
         <button>Generate</button>
@@ -53,8 +70,8 @@ export default function MemeGenerator() {
       
       <div className="meme">
         <img className="img-meme" src={randomImg} alt="meme" />
-        <h3 className="top">{topText}</h3>
-        <h3 className="bottom">{bottomText}</h3>
+        <h3 className="top">{text.topText}</h3>
+        <h3 className="bottom">{text.bottomText}</h3>
       </div>
     </div>
   );
